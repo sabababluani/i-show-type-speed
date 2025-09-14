e577df30681dd0aadd8cf90476c2db236bfc8b46
Saba Babluani <Saba.bablo@gmail.com>
2025-09-14 15:57:07 +0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import TypingBox from "./components/TypingBox";
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-black flex items-center justify-center ">
+    <main className="min-h-screen bg-black flex items-center justify-center">
       <TypingBox />
     </main>
   );
